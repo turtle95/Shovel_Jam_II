f4820012fa946c18c6fa1e6f9e0eb1a0a5116eac
Marissa Lea <ms.marissa.lea@gmail.com>
2018-07-14 01:01:36 -0600

    Finished Menus (Pause/Main)

---
+++ UnityProjectFile/BloodMoon/Assets/Scripts/ResumeEvent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResumeEvent : MonoBehaviour {
+
+	public void Resume(GameObject pauseMenu)
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+    }
+}
