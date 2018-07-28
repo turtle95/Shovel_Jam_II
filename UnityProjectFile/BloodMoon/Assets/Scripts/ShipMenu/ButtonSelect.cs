7ad104775a11a6d153b9de4e728a6017f5f24b79
Joshua Watts <joshuawatts3@gmail.com>
2018-07-28 00:23:32 -0600

    Revert "Revert "changes to sound effects""
    This reverts commit f2097f516f1d1cb7cc453770ff285bc18bb8fd5e.

--- UnityProjectFile/BloodMoon/Assets/Scripts/ShipMenu/ButtonSelect.cs
+++ UnityProjectFile/BloodMoon/Assets/Scripts/ShipMenu/ButtonSelect.cs
@@ -7,11 +7,13 @@ public class ButtonSelect : MonoBehaviour {
     public GameObject turnOn;
 
     public GameObject[] turnOff;
-
+    public AudioSource player;
+    public AudioClip sound;
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Bullet"))
         {
+            player.PlayOneShot(sound);
             turnOn.SetActive(true);
 
             for(int i=0; i<turnOff.Length; i++)
